c2ab54cc450625ddbd84456d5b3c55d0fee40e77
Ben Lange <benlange16205@gmail.com>
2026-08-11 21:01:15 +0200

    asdfg

--- script.js
+++ script.js
@@ -2,28 +2,19 @@ const supabaseUrl = 'https://enynvsqywboflqcphpkm.supabase.co/rest/v1/';
 const supabaseKey = 'sb_publishable_gj6uJi6ZxlRp7ZReynYFvQ_we71tYZX';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-    
-    const userName = formData.get('name');
-    const userPassword = formData.get('password');
-
-    console.log('Name submitted:', userName);
-    console.log('Password submitted:', userPassword);
+async function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-        });
+        email: email,
+        password: password,
+    });
 
-        if (error) {
-            alert("Login failed: " + error.message);
-        } else {
-            alert("logged in");
-            console.log("User data:", data);
+    if (error) {
+        alert("Login failed: " + error.message);
+    } else {
+        alert("logged in");
+        console.log("User data:", data);
     }
-});
+}
