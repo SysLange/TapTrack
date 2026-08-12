83f935f0fe3ec2b7443a942bb6c5997718ae4aff
Ben Lange <166803128+SysLange@users.noreply.github.com>
2026-08-12 15:07:01 +0200

    added order directory

---
+++ order/script.js
@@ -0,0 +1,9 @@
+const drinksGrid = document.getElementById('drinks');
+
+let html = `
+  <div class="flex justify-around">
+    <span>id</span>
+    <span>name</span>
+    <span>price</span>
+  </div>
+`;
