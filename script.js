984abb43f477fbfcde9ca7f42c61371b93360c67
Ben Lange <166803128+SysLange@users.noreply.github.com>
2026-08-13 08:30:37 +0200

    code change

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const supabaseUrl = 'https://enynvsqywboflqcphpkm.supabase.co';
 const supabaseKey = 'sb_publishable_gj6uJi6ZxlRp7ZReynYFvQ_we71tYZX';
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
-async function login() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
+async function login(email, password) {
     const { data, error } = await supabaseClient.auth.signInWithPassword({
         email: email,
         password: password,
@@ -17,4 +14,14 @@ async function login() {
         alert("logged in");
         console.log("User data:", data);
     }
+}
+
+function loginRegister() {
+    var email = document.getElementById("stand").value;
+    var password = document.getElementById("password").value;
+    console.log(email, password);
+}
+
+function loginTap() {
+    console.log("login tap")
 }
